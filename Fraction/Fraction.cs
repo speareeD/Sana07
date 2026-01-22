a5e97500dcc2f1e354c90122263a1cdb0f4b588e
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-22 23:03:05 +0200

    Added private methods GCD, Simplify for simplifying fractions and used it in constructor

--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -1,4 +1,6 @@
-﻿namespace Fraction
+﻿using System.Xml.Schema;
+
+namespace Fraction
 {
     public class Fraction
     {
@@ -11,6 +13,7 @@
                 throw new ArgumentException("Denominator cannot equal zero");
             Denominator = denominator;
             Numerator = numerator;
+            Simplify();
         }
 
         public Fraction(int wholeNumber)
@@ -18,7 +21,28 @@
             Numerator = wholeNumber;
             Denominator = 1;
         }
+        private void Simplify()
+        {
+            int gcd = GCD(Math.Abs(Numerator), Math.Abs(Denominator));
+            Numerator /= gcd;
+            Denominator /= gcd;
 
+            if (Denominator < 0)
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+        }
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
         public override string ToString()
         {
             return $"{Numerator}/{Denominator}";
