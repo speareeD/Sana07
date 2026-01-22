92f7e375e83eb95ea310fc363b908e51784e2ec7
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-22 22:40:53 +0200

    Added constructors for one and two numbers. Added override for ToString() method

--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -4,5 +4,24 @@
     {
         public int Numerator { get; private set; }
         public int Denominator { get; private set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentException("Denominator cannot equal zero");
+            Denominator = denominator;
+            Numerator = numerator;
+        }
+
+        public Fraction(int wholeNumber)
+        {
+            Numerator = wholeNumber;
+            Denominator = 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
     }
 }
