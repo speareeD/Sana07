523477b7b7b4e214c89ab762c5e4e899745e76d5
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-01-24 09:46:38 +0200

    Showed in console each of operators' result

--- Fraction/Program.cs
+++ Fraction/Program.cs
@@ -4,7 +4,24 @@
     {
         private static void Main(string[] args)
         {
-
+            Fraction one = new Fraction(1, 4);
+            Fraction two = new Fraction(4, 8);
+            Fraction three = -one;
+            Console.WriteLine($"+{one} = {+one}");
+            Console.WriteLine($"-({one}) = {-one}");
+            Console.WriteLine($"{one} + {two} = {one + two}");
+            Console.WriteLine($"{one} - {two} = {one - two}");
+            Console.WriteLine($"{one} * {two} = {one * two}");
+            Console.WriteLine($"{one} / {two} = {one / two}");
+            Console.WriteLine($"{one} == {two} : {one == two}");
+            Console.WriteLine($"{one} != {two} : {one != two}");
+            Console.WriteLine($"{one} > {two} : {one > two}");
+            Console.WriteLine($"{one} < {two} : {one < two}");
+            Console.WriteLine($"{one} >= {two} : {one >= two}");
+            Console.WriteLine($"{one} <= {two} : {one <= two}");
+            Console.WriteLine($"{one}++ = {++one}");
+            Console.WriteLine($"{two}-- = {--two}");
+            Console.WriteLine($"{one} = {(double)one}");
         }
     }
 }
